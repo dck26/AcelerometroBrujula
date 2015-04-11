bd541ef6f3ea29dbfd31613934b1df62458c7fef
dck26 <daniel.cuadra@gmail.com>
2015-04-11 10:41:09 -0500

    1

---
+++ js/acelerometro.js
@@ -0,0 +1,27 @@
+//acelerómetro
+var ac = {
+    watchID: null,
+    satisfactorio: function(acc){
+
+        $('#acelerometro h2').html('X: '+ acc.x + '<br>' +
+                                   'Y: '+ acc.y + '<br>' +
+                                   'Z: '+ acc.z);
+},
+    error: function (err){
+        alert('Acelerómetro Error: '+ err.code);
+    },
+    opc: {frequency: 500},
+        
+    iniciar: function(){
+            if(ac.watchID == null){
+            ac.watchID = navigator.accelerometer.watchAcceleration(ac.satisfactorio, ac.error, ac.opc);
+            }
+    },
+    detener: function(){
+        if(ac.watchID){
+            navigator.accelerometer.clearWatch(ac.watchID);
+            ac.watchID = null;
+            $('#acelerometro h2').html('Detenido');
+        }
+    }
+};
